df940eca2fbd708d9529af56ca95e1ad20f6d063
krSeonghyeon <ya41921174@gmail.com>
2024-07-22 18:57:57 +0900

    feat: add Seller Entity

---
+++ src/main/java/com/bcsd/shop/domain/Seller.java
@@ -0,0 +1,31 @@
+package com.bcsd.shop.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "seller", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "user_id", name = "UK_SELLER_USER"),
+        @UniqueConstraint(columnNames = "business_number", name = "UK_SELLER_BUSINESS_NUMBER")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne // 데이터베이스 ON DELETE CASCADE, 양방향 관계 회피
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Column(name = "company_name", nullable = false, length = 100)
+    private String companyName;
+
+    @Column(name = "business_number", nullable = false, length = 20)
+    private String businessNumber;
+}
