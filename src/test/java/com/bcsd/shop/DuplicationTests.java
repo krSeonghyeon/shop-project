3d4c89c4044ff2e54e082eda31eaef2de5f908a9
krSeonghyeon <ya41921174@gmail.com>
2024-08-10 22:33:19 +0900

    feat: 결제 중복방지 테스트코드 추가

--- src/test/java/com/bcsd/shop/DuplicationTests.java
+++ src/test/java/com/bcsd/shop/DuplicationTests.java
@@ -1,5 +1,6 @@
 package com.bcsd.shop;
 
+import com.bcsd.shop.controller.dto.request.PaymentCreateRequest;
 import com.bcsd.shop.controller.dto.request.UserJoinRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,6 +46,29 @@ class DuplicationTests {
 
         // 중복 회원가입 시도
         mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andExpect(status().isConflict());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("결제 중복 방지 테스트")
+    void 결제_중복_방지_테스트() throws Exception {
+        PaymentCreateRequest request = new PaymentCreateRequest(
+                "sdfasfsdafs1231",
+                1000000L,
+                "카드"
+        );
+
+        // 첫번째 결제
+        mockMvc.perform(post("/payments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        ).andExpect(status().isCreated());
+
+        // 중복 결제 시도
+        mockMvc.perform(post("/payments")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(request))
         ).andExpect(status().isConflict());
